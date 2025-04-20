de1ca3398b4c27b837004b61e10a94812c9b6864
Priyanshu sharma <priyanshus8282@gmail.com>
2025-04-20 19:33:10 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 4000,
-  }
-})
+  },
+  plugins: [react(), tailwindcss()],
+  envDir: 'config/',
+});
